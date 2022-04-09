25645fdd3e98bc55bd2e171ee903b45ecced3f43
basit <basitng2004@gmail.com>
2022-04-09 05:18:17 -0700

    made changes

---
+++ js/app.js
@@ -0,0 +1,6 @@
+if (problem_is_challenging) {
+  const generate_100_solutions = Math.random(1000) * RTCDataChannelEvent();
+  console.log("Problem Solved in 100mms", generate_100_solutions);
+} else {
+  console.log("No worries we got you covered");
+}
